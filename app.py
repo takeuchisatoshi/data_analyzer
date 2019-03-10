e8e86924c8e983c9d173927d2fb6d82389cd6fcc
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 14:21:19 +0900

    ユーザーからの入力を整数リストに

---
+++ app.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    # ユーザーからの入力を受け取る
+    input_data = input("データを入力してください(スペース区切り) > ")
+
+    print(input_data)
+    # 文字列リストに変換
+    numbers_as_str = input_data.split(" ")
+    print(numbers_as_str)
+
+    # 整数リストに変換する
+    numbers = []  # 空のリストを作成
+    for number_as_str in numbers_as_str:
+        int_num = int(number_as_str)
+
+        numbers.append(int_num)
+
+    print(numbers)
+
+    # 各統計量を計算する(合計, 平均, ...)
+
+    # ユーザーに見やすいようにフォーマットする
+
+    # 出力する
